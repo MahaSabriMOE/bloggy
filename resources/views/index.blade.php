85a63096e9f57c2ee79225b224fae643e2df262d
MahaSabriMOE <maha.sabri@moe.edu.ps>
2023-06-12 13:48:24 +0300

    m3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Welcome From Index! yes</h1>
+<style>
+    .hero-bg-image{
+        background: url('https://img.freepik.com/free-vector/network-mesh-wire-digital-technology-background_1017-27428.jpg?q=10&h=200' ) no-repeat center center/cover;
+        background-attachment: fixed;
+        height: 600px;
+        
+        }
+</style>
+<div class="hero-bg-image flex flex-col items-center justify-center">
+    <h1>Welcome to my Blog...</h1>
+    <a href="/">Start Reading</a>
+</div>
 
 @endsection
